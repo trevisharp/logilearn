4e6a476a0e9098bfcb91bc60b26cdd7b9755764c
trevisharp <leonardotrevisansilio@gmail.com>
2026-01-27 09:31:44 -0300

    add main simulation empty page;

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,11 +1,61 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+
+  <header>
+    <div class="navigation">
+
+      <div class="navigation-item">
+        <h3>
+          LogiLearn
+        </h3>
+      </div>
+      
+    </div>
+  </header>
+
+  <main>
+    <RouterView/>
+  </main>
+
 </template>
 
-<style scoped></style>
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Quicksand:wght@300..700&display=swap');
+
+.navigation {
+  height: 100%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+}
+
+.navigation-item {
+  margin: 20px;
+}
+
+header {
+  color: white;
+  background-color: rgb(12, 12, 12);
+  width: 100vw;
+  height: 80px;
+  
+  font-family: "Quicksand", sans-serif;
+  font-optical-sizing: auto;
+  font-weight: 16;
+  font-style: normal;
+
+}
+
+main {
+  color: white;
+  background-color: rgb(25, 25, 25);
+  width: 100vw;
+  height: calc(100vh - 80px);
+  
+  font-family: "Quicksand", sans-serif;
+  font-optical-sizing: auto;
+  font-weight: 12;
+  font-style: normal;
+}
+</style>
--- ui/src/router/index.ts
+++ ui/src/router/index.ts
@@ -1,8 +1,11 @@
+import SimulationView from '@/view/SimulationView.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    { path: "/simulation", component: SimulationView }
+  ],
 })
 
 export default router
