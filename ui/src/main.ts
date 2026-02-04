156f991a3789603559ea0bfaf2d2847487551990
trevisharp <leonardotrevisansilio@gmail.com>
2026-02-04 15:50:12 -0300

    add konva

--- ui/src/main.ts
+++ ui/src/main.ts
@@ -3,10 +3,12 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import VueKonva from 'vue-konva'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(VueKonva)
 
 app.mount('#app')
