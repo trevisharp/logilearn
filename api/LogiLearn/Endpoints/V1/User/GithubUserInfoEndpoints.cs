c9deb90ab85d19fb9805b913fedc0a318028c0e9
trevisharp <leonardotrevisansilio@gmail.com>
2026-02-24 10:11:46 -0300

    configure to deploy

--- api/LogiLearn/Endpoints/V1/User/GithubUserInfoEndpoints.cs
+++ api/LogiLearn/Endpoints/V1/User/GithubUserInfoEndpoints.cs
@@ -20,8 +20,7 @@ public static class GithubUserInfoEndpoints
             var client = builder.GetClient(token);
 
             var response = await client.GetAsync("/user");
-
-            Console.WriteLine("oi");
+            
             var content = await response.Content.ReadFromJsonAsync<GithubUserData>();
             return Results.Ok(content);
         });
